4fe3f9ea7ba4d5036bf3fddf6f198cb138794de0
romeB92 <raghmael@gmail.com>
2023-07-01 22:53:44 +0000

    finished task project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+//add into array = concat
+//delete from an array = filter
+//update = map
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [mylist, setMyList] = useState([]);
+
+  return (
+    <div className="container">
+      <h1>My List</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyUpCapture={(e) => {
+              if (e.key === "Enter") {
+                console.log("enter was pressed");
+                setMyList(mylist.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="Write Here"
+          ></input>
+        </li>
+        {mylist.map((item, index) => (
+          <li>
+            {item}{" "}
+            <i
+              className="fas fa-solid fa-trash"
+              onClick={() =>
+                setMyList(
+                  mylist.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            ></i>
+          </li>
+        ))}
+      </ul>
+      <div>{mylist.length} tasks</div>
+    </div>
+  );
 };
 
 export default Home;
